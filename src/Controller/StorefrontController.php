3a0145358b7e6f63d4379a9b9920a7f9ca49d103
Phil G Home <philipp.georg@webacumen.de>
2019-08-30 23:51:23 +0200

    routescope

--- src/Controller/StorefrontController.php
+++ src/Controller/StorefrontController.php
@@ -17,7 +17,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 
+
+/**
+ * @RouteScope(scopes={"storefront"})
+ */
 class StorefrontController extends \Shopware\Storefront\Controller\StorefrontController
 {
 
