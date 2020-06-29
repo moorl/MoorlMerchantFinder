355f82e9406332776a631be41c7ab7ed7a507e8f
moori <info@moorleiche.com>
2020-06-29 23:06:10 +0200

    dev

--- src/Resources/app/administration/src/module/moorl-merchant-finder/index.js
+++ src/Resources/app/administration/src/module/moorl-merchant-finder/index.js
@@ -1,8 +1,20 @@
-const { Module } = Shopware;
+const { Module, Application } = Shopware;
 import './page/moorl-merchant-finder-list';
 import './page/moorl-merchant-finder-detail';
 import './page/moorl-merchant-finder-create';
+import './search-service';
 import './style/main.scss';
+
+Application.addServiceProviderDecorator('searchTypeService', searchTypeService => {
+    searchTypeService.upsertType('moorl_merchant', {
+        entityName: 'moorl_merchant',
+        entityService: 'moorlMerchantService',
+        placeholderSnippet: 'moorl-merchant-finder.general.placeholderSearchBar',
+        listingRoute: 'moorl.merchant.finder.list'
+    });
+
+    return searchTypeService;
+});
 
 Module.register('moorl-merchant-finder', {
     type: 'plugin',
@@ -10,6 +22,7 @@ Module.register('moorl-merchant-finder', {
     title: 'moorl-merchant-finder.general.mainMenuItemGeneral',
     color: '#ff3d58',
     icon: 'default-object-globe',
+    entity: 'moorl_merchant',
     routes: {
         list: {
             component: 'moorl-merchant-finder-list',
---
+++ src/Resources/app/administration/src/module/moorl-merchant-finder/search-service/index.js
@@ -0,0 +1,7 @@
+const { Component } = Shopware;
+
+import template from './index.html.twig';
+
+Component.override('sw-search-bar-item', {
+    template
+})
