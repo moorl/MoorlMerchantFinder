ea9bf2212ca24c97babc5d75cb592d6a739fd9bc
moori <info@moorleiche.com>
2021-02-07 22:16:06 +0100

    dev

---
+++ src/Migration/Migration1612728896.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Moorl\MerchantFinder\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\InheritanceUpdaterTrait;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1612728896 extends MigrationStep
+{
+    use InheritanceUpdaterTrait;
+
+    public function getCreationTimestamp(): int
+    {
+        return 1612728896;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $sql = <<<SQL
+ALTER TABLE `moorl_merchant_stock`
+ADD `product_version_id` binary(16) NULL AFTER `product_id`;
+SQL;
+
+        $connection->executeQuery($sql);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
